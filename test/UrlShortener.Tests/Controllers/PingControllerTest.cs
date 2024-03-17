1e21062bc52d4a84242f68b2c6b680d1769a4523
Ravi <iamrks@outlook.com>
2024-03-17 22:26:48 +0530

    Added Integration Tests Support

---
+++ test/UrlShortener.Tests/Controllers/PingControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlShortener.Tests.Controllers
+{
+    public class PingControllerTest : IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        private readonly CustomWebApplicationFactory<Program> _factory;
+
+        public PingControllerTest(CustomWebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Get_ReturnCorrectEnvironment()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("api/ping");
+            response.EnsureSuccessStatusCode();
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal("Staging", responseBody);
+        }
+    }
+}
---
+++ test/UrlShortener.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data.Common;
+using System.Text;
+
+namespace UrlShortener.Tests
+{
+    public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Staging");
+
+            builder.ConfigureAppConfiguration((ctx, config) =>
+            {
+                var keyValuePairs = Enumerable.Empty<KeyValuePair<string, string?>>()
+                    .Append(new KeyValuePair<string, string?>("HttpClient:Github:Token", ""))
+                    .Append(new KeyValuePair<string, string?>("Logging:Seq:ServerUrl", ""))
+                    .Append(new KeyValuePair<string, string?>("Logging:Seq:ApiKey", ""));
+
+                config.AddInMemoryCollection(keyValuePairs);
+            });
+
+
+            builder.ConfigureServices(services =>
+            {
+                var dbContextDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+
+                if (dbContextDescriptor is not null) services.Remove(dbContextDescriptor);
+
+                var dbConnectionDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbConnection));
+
+                if (dbConnectionDescriptor is not null) services.Remove(dbConnectionDescriptor);
+
+                UseInMemoryDatabase<ApplicationDbContext>(services);
+            });
+        }
+
+        private void UseInMemoryDatabase<TContext>(IServiceCollection services) where TContext : DbContext
+        {
+            services.AddDbContext<TContext>(options => { options.UseInMemoryDatabase("InMemoryDbForTesting"); });
+        }
+    }
+}
